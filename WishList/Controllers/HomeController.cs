248dd2304a24f0fc731b98211525d431e2e44577
Pardeep Chahal <pardeepchahal89@gmail.com>
2022-01-01 14:59:34 +0530

    Fix for index view

--- WishList/Controllers/HomeController.cs
+++ WishList/Controllers/HomeController.cs
@@ -11,7 +11,7 @@ namespace WishList.Controllers
     {
         public IActionResult Index()
         {
-            return View();
+            return View("Index");
         }
 
         public IActionResult Error()
