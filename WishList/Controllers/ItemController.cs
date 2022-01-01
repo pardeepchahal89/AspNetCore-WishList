2a6a97a1a085f2dd1735945ab36a432a0c1e3bdf
Pardeep Chahal <pardeepchahal89@gmail.com>
2022-01-01 15:24:47 +0530

    build fix

--- WishList/Controllers/ItemController.cs
+++ WishList/Controllers/ItemController.cs
@@ -35,7 +35,7 @@ namespace WishList.Controllers
             _context.Items.Add(item);
             _context.SaveChanges();
 
-            return RedirectToAction("Index")
+            return RedirectToAction("Index");
         }
     }
 }
